ed0fde93f2e7d84d8440a37664b39ef71fe87663
jkjh8 <jkjh82@naver.com>
2022-08-06 01:08:27 +0900

    12

--- quasar.config.js
+++ quasar.config.js
@@ -48,7 +48,7 @@ module.exports = configure(function (ctx) {
 
       // transpile: false,
       // publicPath: '../bs/public',
-      distDir: '../bs/public/spa',
+      // distDir: '../bs/public/spa',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
--- src/composables/useAxios.js
+++ src/composables/useAxios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: process.env.DEV
     ? `http://${window.location.hostname}:3000/api`
-    : `https://${window.location.hostname}/api`,
+    : `https://${window.location.hostname}:3443/api`,
   withCredentials: true
 })
 
