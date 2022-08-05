8728213c150e7a7540000d0a8c3d680309a33614
jkjh8 <jkjh82@naver.com>
2022-08-05 10:52:07 +0900

    build path change

--- quasar.config.js
+++ quasar.config.js
@@ -47,7 +47,8 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      // publicPath: '../bs/public',
+      distDir: '../bs/public/spa',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
